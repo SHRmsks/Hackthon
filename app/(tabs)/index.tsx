75efce81377f635ab3cca94b8f18d157c8e28192
SHR <SHR@crc-dot1x-nat-10-239-94-116.bu.edu>
2024-11-02 10:34:21 -0400

    frontend

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,117 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React, { useState, useRef, useEffect} from "react";
+import { StyleSheet, Text, View, Button, TouchableOpacity,Image } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { CameraType, CameraView, CameraViewRef, useCameraPermissions } from "expo-camera";
 
-export default function HomeScreen() {
+const App = () => {
+  const [type, setType] = useState<CameraType>("front");
+  const [permission, setPermission] = useCameraPermissions();
+  
+  const onpress = ()=> {
+   type ==="front"? setType("back"): setType("front"); 
+  
+  }
+  const CameraRef = useRef<CameraView | null>(null);  
+  
+  const takingFrame  = async ()=> {
+    if (CameraRef.current){
+        let photo = await CameraRef.current.takePictureAsync({base64: true, quality:0.8, skipProcessing: true}); 
+        console.log ("hello"); 
+        console.log(photo?.width);
+    } 
+  }
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+  if (!permission?.granted) {
+    <View style={styles.warning}>
+      <Text style={styles.text}>we need your permission for camera</Text>
+      <Button title="Grant permission" onPress={setPermission}></Button>
+    </View>;
+  }
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <LinearGradient
+      colors={["#82edb1", "#3ca63c", "#10753c"]} // Bright colors to test visibility
+      style={styles.gradient}
+    >
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.container}>
+          <CameraView facing={type} style={styles.camera}
+          ref={CameraRef}
+          onCameraReady={takingFrame}
+          
+          >
+
+
+          <TouchableOpacity style= {styles.button} onPress={onpress}> 
+                <View style={styles.container}>
+                  <Image source={require('@assets/images/flip.png')} style= {styles.icons}  resizeMode="contain"/>
+                </View>
+              </TouchableOpacity>
+          </CameraView>
+          
+          <View > 
+              
+            
+             </View>
+
+        </View>
+      </SafeAreaView>
+    </LinearGradient>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  warning: {
+    flex: 0.5,
+    justifyContent: "center",
+    backgroundColor: "white",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+
+  safeArea: {
+    flex: 1,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  camera: {
+    flex: 1,
+    width: "100%",
+    height:"100%",
+    flexDirection: "column",
+    justifyContent:"flex-end",
+    paddingVertical:20,  
+    alignItems: "center",
+   
+
   },
+  gradient: {
+    flex: 1,
+    width: "100%",
+  },
+  button: {
+    height: 50,
+    width: 120,
+    justifyContent: "center",
+   
+    backgroundColor: "#7bb096", 
+    borderRadius: 20, 
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 10,
+    fontWeight: "bold",
+  },
+
+  icons:{
+    flex: 1, 
+    
+  }
 });
+
+export default App;
